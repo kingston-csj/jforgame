e013cf4702b54238e6bfd43488f6a335b9553c57
definemyself <definemyself@163.com>
2017-11-27 22:15:26 +0800

    add simpleObjectPool util

---
+++ jforgame-common/src/main/java/com/kingston/jforgame/common/utils/MemoryObject.java
@@ -0,0 +1,9 @@
+package com.kingston.jforgame.common.utils;
+
+public interface MemoryObject {
+
+	/** 
+	 * 释放相关引用
+	 */
+	void release();
+}
---
+++ jforgame-common/src/main/java/com/kingston/jforgame/common/utils/SimplyObjectPool.java
@@ -0,0 +1,32 @@
+package com.kingston.jforgame.common.utils;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class SimplyObjectPool <E extends MemoryObject> {
+	
+	private int capacity;
+	
+	private LinkedBlockingQueue<E> queue = new LinkedBlockingQueue<>();
+	
+	public SimplyObjectPool(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public E borrowObject(Class<? extends MemoryObject> prototype) throws InstantiationException, IllegalAccessException {
+		E object = queue.poll();
+		if (object != null) {
+			return object;
+		}
+		return (E) prototype.newInstance();
+	}
+	
+	public void returnObject(E object) {
+		if (this.queue.size() < capacity) {
+			object.release();
+			this.queue.offer(object);
+		}
+	}
+	
+
+}
