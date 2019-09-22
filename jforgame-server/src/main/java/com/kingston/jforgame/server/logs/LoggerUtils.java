8898814815cc2da251111008c0e5db8fa195f983
kingston-csj <475139136@qq.com>
2019-09-22 16:33:06 +0800

    增加log4j动态日志

--- jforgame-server/src/main/java/com/kingston/jforgame/server/logs/LoggerUtils.java
+++ jforgame-server/src/main/java/com/kingston/jforgame/server/logs/LoggerUtils.java
@@ -7,19 +7,14 @@ public class LoggerUtils {
 	/**
 	 * Log an exception at the ERROR level with an
 	 * accompanying message.
-	 *
-	 * @param msg the message accompanying the exception
-	 * @param t   the exception to log
 	 */
 	public static void error(String errMsg, Exception e) {
 		Logger logger = LoggerSystem.EXCEPTION.getLogger(); 
-		logger.error("", e);  
+		logger.error(errMsg, e);
 	}
 	
 	/**
 	 * @param format the format string
-     * @param arg1   the first argument
-     * @param arg2   the second argument
      */
 	public static void error(String format, Object... arguments) {
 		Logger logger = LoggerSystem.EXCEPTION.getLogger(); 
