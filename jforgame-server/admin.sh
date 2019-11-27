6caade1d2bd0904fe104f9e60134aff479ed2862
kingston-csj <definemyself@163.com>
2019-11-27 14:42:22 +0800

    优化运维脚本

--- jforgame-server/admin.sh
+++ jforgame-server/admin.sh
@@ -1,12 +1,21 @@
  #!/bin/sh
 serverId="001"
+GAME_PID=game_${serverId}.pid
+JMX_IP=`ifconfig eth0 | grep "inet addr:" |awk '{print $2}' | cut -c 6-`
 
-JVM_ARGS='-Xms1024m -Xmx1024m -Xmn512m -XX:MaxTenuringThreshold=3 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=2 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:-OmitStackTraceInFastThrow -XX:+PrintTenuringDistribution' 
+JVM_ARGS="-Xms1024m -Xmx1024m -Xmn512m -XX:MaxTenuringThreshold=3"
+JVM_ARGS="$JVM_ARGS="" -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=2 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:-OmitStackTraceInFastThrow -XX:+PrintTenuringDistribution" 
 JVM_ARGS="$JVM_ARGS="" -Dcom.sun.management.jmxremote.port=10086"
 JVM_ARGS="$JVM_ARGS="" -Dcom.sun.management.jmxremote.authenticate=false"
 JVM_ARGS="$JVM_ARGS="" -Dcom.sun.management.jmxremote.ssl=false"
+JVM_ARGS="$JVM_ARGS="" -Djava.rmi.server.hostname="$JMX_IP
 
 if [ $1 == "start" ]; then
+  exist=`ls -l /var/tmp/ | grep ${GAME_PID}`
+  if [[ $exist != '' ]]; then
+    echo "server had started"
+    exit 0
+  fi
   cd target/
   serverName='GameServer'
   localdir=../../gc
@@ -19,11 +28,13 @@ if [ $1 == "start" ]; then
   -Xloggc:$localdir/gc_$today.log \
   -Dgame.serverId=$serverId \
   -Dfile.encoding=UTF-8 -jar $serverName.jar > /dev/null &
+    echo $! > /var/tmp/${GAME_PID}
 elif [ $1 == "stop" ]; then 
-  pid=`jps -lv|grep serverId=$serverId|awk '{print $1}'`
+  #pid=`jps -lv|grep serverId=$serverId|awk '{print $1}'`
+  pid=`cat /var/tmp/${GAME_PID}`
   if [ $pid > 0 ]; then   
      echo "get ready to close server"
-     kill 15 $pid
+     kill -15 $pid
      echo "server closed successfully"
   fi
 elif [ $1 == "update" ]; then 
