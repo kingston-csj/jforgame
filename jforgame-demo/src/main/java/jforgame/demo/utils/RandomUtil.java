8fb8a3c62b7a6ca8ac7f1299e1be0185bcc109f7
CarsonQizhao <441376018@qq.com>
2025-07-14 17:02:43 +0800

    完善RandomUtil方法

--- jforgame-demo/src/main/java/jforgame/demo/utils/RandomUtil.java
+++ jforgame-demo/src/main/java/jforgame/demo/utils/RandomUtil.java
@@ -1,9 +1,14 @@
 package jforgame.demo.utils;
 
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Created by qizhao.liao 2019/9/29
+ * Created by Carson
  */
 public class RandomUtil {
 
@@ -25,5 +30,81 @@ public class RandomUtil {
     public static final int nextInt() {
         return getRandom().nextInt();
     }
+
+    public static int randomValue(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("min > max");
+        }
+        if (min == max) {
+            return min;
+        }
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+
+    public static int randomList( final List<Integer> probabilityList ) {
+        if (CollectionUtils.isEmpty(probabilityList)) {
+            throw new IllegalArgumentException("probabilityList is empty");
+        }
+        for (Integer i : probabilityList) {
+            if (i < 0) {
+                throw new IllegalArgumentException("probabilityList contains negative number");
+            }
+        }
+        return randomIndex(probabilityList);
+    }
+
+    public static int randomIndex(List<Integer> probs) {
+        if (probs == null || probs.isEmpty()) {
+            return -1;
+        }
+        int sum = probs.stream().reduce(0, Integer::sum);
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        // 生成一个在 [0, sum) 范围内的随机数
+        int randomValue = random.nextInt(sum);
+        int accumulated = 0;
+        // 遍历概率列表，通过累计概率来判断随机数落在哪个区间，从而确定索引
+        for (int i = 0; i < probs.size(); i++) {
+            accumulated += probs.get(i);
+            if (randomValue < accumulated) {
+                return i;
+            }
+        }
+        // 理论不会跑到这里，语法需求
+        throw new IllegalArgumentException("randomIndex out of range");
+    }
+
+    public static List<Integer> randomList(List<Integer> probabilityList, int count, boolean remove) {
+        if (CollectionUtils.isEmpty(probabilityList)) {
+            throw new IllegalArgumentException("probabilityList is empty");
+        }
+        for (Integer prob : probabilityList) {
+            if (prob < 0) {
+                throw new IllegalArgumentException("probabilityList contains negative number");
+            }
+        }
+        if (count <= 0) {
+            throw new IllegalArgumentException("count <= 0");
+        }
+        if (remove && count > probabilityList.size()) {
+            throw new IllegalArgumentException("count > probabilityList size");
+        }
+        List<Integer> hits = new ArrayList<>(count);
+        if (remove) {
+            for (int i = 0; i < count; i++) {
+                int index = randomIndex(probabilityList);
+                hits.add(index);
+                probabilityList.set(index, 0);
+            }
+        } else {
+            for (int i = 0; i < count; i++) {
+                int index = randomIndex(probabilityList);
+                hits.add(index);
+            }
+        }
+        return hits;
+    }
+
+//    public static <E> List<E> randomWeightList()
 
 }
